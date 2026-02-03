aed08c6dd76fbb4af300ab364b6717f8d366c02c
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2026-02-03 16:27:27 +0100

    Clear Swagger Usless

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ * title="Cerca Terapista API",
+ * version="1.0.0",
+ * description="Documentazione ufficiale delle API per Cerca Terapista",
+ * @OA\Contact(
+ * email="admin@example.com"
+ * )
+ * )
+ * @OA\Server(
+ * url="http://localhost:8000",
+ * description="Server Locale"
+ * )
+ */
 abstract class Controller
 {
     //
